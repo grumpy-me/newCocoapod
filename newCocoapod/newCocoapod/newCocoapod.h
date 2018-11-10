82a73203300c74f8dcd8cc8fc60d189dee9bba6b
Apple <meghajppatil@gmail.com>
2018-11-10 10:31:26 +0530

    Finished our framework

---
+++ newCocoapod/newCocoapod/newCocoapod.h
@@ -0,0 +1,19 @@
+//
+//  newCocoapod.h
+//  newCocoapod
+//
+//  Created by Apple on 10/11/18.
+//  Copyright © 2018 SampleLearning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for newCocoapod.
+FOUNDATION_EXPORT double newCocoapodVersionNumber;
+
+//! Project version string for newCocoapod.
+FOUNDATION_EXPORT const unsigned char newCocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <newCocoapod/PublicHeader.h>
+
+
---
+++ newCocoapod/newCocoapod/service.swift
@@ -0,0 +1,9 @@
+//
+//  service.swift
+//  newCocoapod
+//
+//  Created by Apple on 10/11/18.
+//  Copyright © 2018 SampleLearning. All rights reserved.
+//
+
+import Foundation
